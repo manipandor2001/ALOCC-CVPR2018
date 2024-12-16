70ab642063bdbb5eaaba61563114cfb27c0f2d6f
Manish Pandor <109963821+manipandor2001@users.noreply.github.com>
2024-12-16 22:00:43 +0530

    Update ops.py

--- ops.py
+++ ops.py
@@ -10,99 +10,100 @@ from tensorflow.python.framework import ops
 
 from utils import *
 
-try:
-  image_summary = tf.image_summary
-  scalar_summary = tf.scalar_summary
-  histogram_summary = tf.histogram_summary
-  merge_summary = tf.merge_summary
-  SummaryWriter = tf.train.SummaryWriter
-except:
-  image_summary = tf.summary.image
-  scalar_summary = tf.summary.scalar
-  histogram_summary = tf.summary.histogram
-  merge_summary = tf.summary.merge
-  SummaryWriter = tf.summary.FileWriter
+import tensorflow as tf
 
-if "concat_v2" in dir(tf):
-  def concat(tensors, axis, *args, **kwargs):
-    return tf.concat_v2(tensors, axis, *args, **kwargs)
-else:
-  def concat(tensors, axis, *args, **kwargs):
+
+# TensorFlow 2.x summarization replacements
+#image_summary = tf.summary.image
+#scalar_summary = tf.summary.scalar
+#histogram_summary = tf.summary.histogram
+#merge_summary = tf.summary.merge
+#SummaryWriter = tf.summary.create_file_writer
+
+
+# Concatenation remains the same in TensorFlow 2.x
+def concat(tensors, axis, *args, **kwargs):
     return tf.concat(tensors, axis, *args, **kwargs)
 
-class batch_norm(object):
-  def __init__(self, epsilon=1e-5, momentum = 0.9, name="batch_norm"):
-    with tf.variable_scope(name):
-      self.epsilon  = epsilon
-      self.momentum = momentum
-      self.name = name
 
-  def __call__(self, x, train=True):
-    return tf.contrib.layers.batch_norm(x,
-                      decay=self.momentum, 
-                      updates_collections=None,
-                      epsilon=self.epsilon,
-                      scale=True,
-                      is_training=train,
-                      scope=self.name)
+# Batch normalization using TensorFlow 2.x's tf.keras.layers.BatchNormalization
+class batch_norm:
+    def __init__(self, epsilon=1e-5, momentum=0.9, name="batch_norm"):
+        self.batch_norm_layer = tf.keras.layers.BatchNormalization(
+            epsilon=epsilon, momentum=momentum, name=name
+        )
 
+    def __call__(self, x, train=True):
+        return self.batch_norm_layer(x, training=train)
+
+
+# Concatenate conditioning vector on the feature map axis
 def conv_cond_concat(x, y):
-  """Concatenate conditioning vector on feature map axis."""
-  x_shapes = x.get_shape()
-  y_shapes = y.get_shape()
-  return concat([
-    x, y*tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])], 3)
+    x_shapes = tf.shape(x)
+    y_shapes = tf.shape(y)
+    y_tiled = tf.tile(y, [x_shapes[0], x_shapes[1], x_shapes[2], 1])
+    return tf.concat([x, y_tiled], axis=-1)
 
-def conv2d(input_, output_dim, 
-       k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
-       name="conv2d"):
-  with tf.variable_scope(name):
-    w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim],
-              initializer=tf.truncated_normal_initializer(stddev=stddev))
-    conv = tf.nn.conv2d(input_, w, strides=[1, d_h, d_w, 1], padding='SAME')
 
-    biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
-    conv = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
+# 2D Convolution
+def conv2d(input_, output_dim, k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02, name="conv2d"):
+    initializer = tf.random_normal_initializer(stddev=stddev)
+    conv_layer = tf.keras.layers.Conv2D(
+        filters=output_dim,
+        kernel_size=(k_h, k_w),
+        strides=(d_h, d_w),
+        padding="same",
+        kernel_initializer=initializer,
+        bias_initializer=tf.zeros_initializer(),
+        name=name,
+    )
+    return conv_layer(input_)
 
-    return conv
 
-def deconv2d(input_, output_shape,
-       k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
-       name="deconv2d", with_w=False):
-  with tf.variable_scope(name):
-    # filter : [height, width, output_channels, in_channels]
-    w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
-              initializer=tf.random_normal_initializer(stddev=stddev))
-    
-    try:
-      deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape,
-                strides=[1, d_h, d_w, 1])
-
-    # Support for verisons of TensorFlow before 0.7.0
-    except AttributeError:
-      deconv = tf.nn.deconv2d(input_, w, output_shape=output_shape,
-                strides=[1, d_h, d_w, 1])
-
-    biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
-    deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
-
+# 2D Transposed Convolution
+def deconv2d(
+    input_,
+    output_shape,
+    k_h=5,
+    k_w=5,
+    d_h=2,
+    d_w=2,
+    stddev=0.02,
+    name="deconv2d",
+    with_w=False,
+):
+    initializer = tf.random_normal_initializer(stddev=stddev)
+    deconv_layer = tf.keras.layers.Conv2DTranspose(
+        filters=output_shape[-1],
+        kernel_size=(k_h, k_w),
+        strides=(d_h, d_w),
+        padding="same",
+        kernel_initializer=initializer,
+        bias_initializer=tf.zeros_initializer(),
+        name=name,
+    )
+    result = deconv_layer(input_)
     if with_w:
-      return deconv, w, biases
-    else:
-      return deconv
-     
+        return result, deconv_layer.kernel, deconv_layer.bias
+    return result
+
+
+# Leaky ReLU
 def lrelu(x, leak=0.2, name="lrelu"):
-  return tf.maximum(x, leak*x)
+    return tf.maximum(x, leak * x)
 
+
+# Fully connected linear layer
 def linear(input_, output_size, scope=None, stddev=0.02, bias_start=0.0, with_w=False):
-  shape = input_.get_shape().as_list()
-
-  with tf.variable_scope(scope or "Linear"):
-    matrix = tf.get_variable("Matrix", [shape[1], output_size], tf.float32,
-                 tf.random_normal_initializer(stddev=stddev))
-    bias = tf.get_variable("bias", [output_size],
-      initializer=tf.constant_initializer(bias_start))
+    initializer = tf.random_normal_initializer(stddev=stddev)
+    bias_initializer = tf.constant_initializer(bias_start)
+    dense_layer = tf.keras.layers.Dense(
+        units=output_size,
+        kernel_initializer=initializer,
+        bias_initializer=bias_initializer,
+        name=scope or "Linear",
+    )
+    output = dense_layer(input_)
     if with_w:
-      return tf.matmul(input_, matrix) + bias, matrix, bias
-    else:
-      return tf.matmul(input_, matrix) + bias
+        return output, dense_layer.kernel, dense_layer.bias
+    return output
